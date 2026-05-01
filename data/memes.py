b01163b0607cc241ee7d7e869777bcfc4009ee5b
Матвей <mothalk@yandex.ru>
2026-05-01 13:59:32 +0400

    added showing likes and matches count

--- data/memes.py
+++ data/memes.py
@@ -13,9 +13,11 @@ class Meme(SqlAlchemyBase):
     title = sa.Column(sa.String, nullable=True)
     description = sa.Column(sa.String, nullable=True)
     picture = sa.Column(sa.BLOB)
-    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
+    created_date = sa.Column(sa.DateTime, default=lambda d: datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
     project_type = sa.Column(sa.String)
     meta = sa.Column(sa.JSON)
+    likes = sa.Column(sa.JSON, nullable=True, default=[])
+    matches = sa.Column(sa.JSON, nullable=True, default=[])
 
     user_id = sa.Column(sa.Integer, sa.ForeignKey("users.id"))
     user = orm.relationship('User')
